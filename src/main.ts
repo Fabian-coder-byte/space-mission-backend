a1ca306249a7b70791f6b54ac823d9a5d3306e6e
Fabian sartini <fabiansartini@gmail.com>
2026-05-28 15:35:51 +0200

    aggiunta cors produzione

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://space-mission-fe.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
